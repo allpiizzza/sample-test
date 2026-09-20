1645c8f0968f9074459e9b76f27c875f1fb99f9e
allpiizzza <allpiizza94@gmail.com>
2026-09-20 11:46:35 +0900

    url 변경

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 배포한 Apps Script 웹앱 주소 — 나중에 다시 배포해서 주소가 바뀌면 여기만 고치면 됨
-const API_URL = "https://script.google.com/macros/s/AKfycbzp7zF1y-ltSi9zd7HGWa86d0L8hudmegffRrpUXclpwajFmMU3nf3we59jylAMfy3A/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbz1PWvYjzfKnBdK1-Snd74nWloPeDN3IbUqnDnsxuPuGSvZmQwB5Vbf9tk3R-qz4FKZ/exec";
 
 // 목록 불러오기: Apps Script에 GET 요청을 보내고, 후기 배열을 받아옴
 async function fetchReviews() {
